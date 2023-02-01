1ba339f21bfbd4e0929cb457288db92b9f191bf9
Nordan17 <nahuelgil98@gmail.com7>
2023-02-01 11:35:55 -0300

    Reusable Card component

--- src/app/features/auth/auth.module.ts
+++ src/app/features/auth/auth.module.ts
@@ -4,9 +4,10 @@ import { LoginFormComponent } from './components/login-form/login-form.component
 import { SignUpFormComponent } from './components/sign-up-form/sign-up-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [LoginFormComponent, SignUpFormComponent],
-  imports: [CommonModule, ReactiveFormsModule, RouterModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule, SharedModule],
 })
 export class AuthModule {}
